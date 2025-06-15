08de6c2985a8b663e240be9ae850cbb8b83c10b8
GianES26 <a20224926@pucp.edu.pe>
2025-06-15 12:35:13 -0500

    Primer commit

---
+++ src/test/java/com/example/lab9_20224926/Lab920224926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20224926;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920224926ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
